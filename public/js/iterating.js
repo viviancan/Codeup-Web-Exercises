63ada1c2845c07721dc80480fd0d80e8cb154353
Vivian Canales <viviancans@gmail.com>
2017-06-19 11:07:33 -0500

    Arrays #2- Used for loop and for each loop to log array

--- public/js/iterating.js
+++ public/js/iterating.js
@@ -4,15 +4,14 @@
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
 
 var names = ['David' , 'Lonetta', 'Ari', 'Miriam'];
+	for (var i = 0; i < names.length; i++){
+		console.log("(1)The name is: " + names[i]);
+	}
 
-    // TODO: Create a log statement that will log the number of elements in the names array.
-    console.log(names.length);
-
-    // TODO: Create log statements that will print each of the names array elements individually.
-console.log("The first name is " + names[0]);
-console.log("The first name is " + names[1]);
-console.log("The first name is " + names[2]);
-console.log("The first name is " + names[3]);
+names.forEach(function(element, index, array){
+	console.log("(2)The name is: " + element);
+}
+	)
 
 })();
 
