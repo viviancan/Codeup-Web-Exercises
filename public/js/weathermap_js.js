9a447787028957879b42aa90cc36f20699ff4899
Vivian Canales <viviancans@gmail.com>
2017-07-07 15:46:50 -0500

    Weather App - updated comments and old code removed. Fully functional

--- public/js/weathermap_js.js
+++ public/js/weathermap_js.js
@@ -1,34 +1,15 @@
 (function() {
 	"use strict";
 
-//Ajax request to openWeather API
-	// var requestWeather = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-	// 	APPID: "6ac2f305a43f171cb8e8ad2076b9a183" , 
-	// 	id: 4726206 ,
-	// 	units: "imperial",
-	// 	cnt: 3
-	// });
 
-// //If the request is succusessful, it will call the addWeatherToPage function
-// 	requestWeather.done(function(data){
-// 		console.log(data);
-// 		addWeatherToPage(data);
-// 	});
-
-// //If the request fails, error and status will be console logged
-// 	requestWeather.fail(function(jqXHR, status, error){
-// 			console.log(status);
-// 			console.log(error);
-// 	});
-
-//addWeatherToPage adds weather to page for each day. calls weatherFormat function for proper formatting
+	//addWeatherToPage adds weather to page for each day. calls weatherFormat function for proper formatting
 	function addWeatherToPage(data){
 		data.list.forEach(function(weather){
 			weatherFormat(weather);
 		});
 	}
 
-//weatherFormat formats all the weather data from the API 
+	//weatherFormat formats all the weather data from the API 
 	function weatherFormat(weather){
 		var htmlString = "";
 		htmlString += "<div class='col-md-4'" +
@@ -41,11 +22,11 @@
 			"<p>" + "<strong>Pressure: </strong>" + weather.pressure + "</p>" +
 			"</div>"
 
-//appends weather data & format into the insertWeather div
+	//appends weather data & format into the insertWeather div
 		$("#insertWeather").append(htmlString);
 	};
 
-//convertTime converts timestamp to readble date
+	//convertTime converts timestamp to readble date
 	function convertTime(unix){
 		var dt = unix * 1000;
 		var date = new Date(dt);
@@ -56,18 +37,12 @@
 		return formattedDate; 
 	};
 
-//addCityName adds city name above weather
+	//addCityName adds city name above weather
 	function addCityName(city){
 		$("#cityName").html(city);
 	};
 
-
-
-/*------------------------------------------------------------------------------------------------------------------//	
-													MAP INFORMATION 
-//------------------------------------------------------------------------------------------------------------------*/
-
-//Sets up orginal map
+	//Sets up orginal map
 	function map() {
 
 		//Set default locaton to SATX
@@ -82,7 +57,7 @@
 		//Creates new map with specified map options in 'map' div
 		var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-		//creates marker at default location 
+		//creates draggable marker at default location 
 		var marker = new google.maps.Marker({
 			position: defLocation,
 			map: map,
@@ -91,13 +66,15 @@
 		});
 
 		google.maps.event.addListener(marker, 'dragend', function(){
+
+			//clears the weather display
 			$("#insertWeather").html(" ");
+
+			//Gets lat & long position assigns to variable
 			var latMapMarker = this.getPosition().lat();
 			var longMapMarker = this.getPosition().lng();
 
-			console.log(latMapMarker);
-			console.log(longMapMarker);
-
+			//Makes request to openweather using lat and long from dragged marker
 			var requestWeather = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
 				APPID: "6ac2f305a43f171cb8e8ad2076b9a183" , 
 				lat: latMapMarker ,
@@ -120,59 +97,8 @@
 				console.log(error);
 			});
 		});
-
 		
-	}
+	};
 		map();
-
-
-	// }); 
-
-
-
-/*------------------------------------------------------------------------------------------------------------------//	
-					(Version) User inputs latitude and longitude values --> Returns weather forecast
-//------------------------------------------------------------------------------------------------------------------*/
-
-	// // Makes submit button clickable
-	// $("#submit").click(function(){
-
-	// 	//Resets display divs to empty between clicks
-	// 	$("#insertWeather").html(" ");
-
-	// 	//Stores user input of lat/lng into variables
-	// 	var latInput = $("#latBox").val(); 
-	// 	var longInput = $("#longBox").val();
-
-	// 	console.log("Latitutde: " + latInput + " Longitude: " + longInput);
-	// 	console.log("button clicked");
-
-	// 	var requestWeather = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-	// 		APPID: "6ac2f305a43f171cb8e8ad2076b9a183" , 
-	// 		lat: latInput ,
-	// 		lon: longInput,
-	// 		units: "imperial",
-	// 		cnt: 3
-	// 	});
-
-	// 	//If the request is succusessful, it will call the addWeatherToPage function
-	// 	requestWeather.done(function(data){
-	// 		console.log(data);
-	// 		// $("#cityName").html(data.city.name);
-	// 		addCityName(data.city.name);
-	// 		addWeatherToPage(data);
-	// 	});
-
-	// 	//If the request fails, error and status will be console logged
-	// 	requestWeather.fail(function(jqXHR, status, error){
-	// 		console.log(status);
-	// 		console.log(error);
-	// 	});
-
-	// }); 
-
-
-
-
 
 })();
