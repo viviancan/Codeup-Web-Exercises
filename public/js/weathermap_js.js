59a2e20d0fa54bc4f5f32b00b2b9013de3f9efdc
Vivian Canales <viviancans@gmail.com>
2017-07-13 11:08:50 -0500

    Fixed marker duplication

--- public/js/weathermap_js.js
+++ public/js/weathermap_js.js
@@ -12,7 +12,7 @@
 	function weatherFormat(weather){
 		var htmlString = "";
 		htmlString += "<div class='col-md-4'" +
-			"<p>"+ "<strong>" + convertTime(weather.dt) + "</strong>" + "</p>" +
+			"<p>" + "<strong>" + convertTime(weather.dt) + "</strong>" + "</p>" +
 			"<p>" + weather.temp.max + "°" + " / " + weather.temp.min + "°" + "</p>" +
 			"<p>" + "<img src='http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png'>" + "</p>" +
 			"<p>" + "<strong>" + weather.weather[0].main + "</strong>"+ ": " + weather.weather[0].description + "</p>" +
@@ -41,69 +41,11 @@
 		$("#cityName").html(city);
 	};
 
-	//Sets up orginal map
-	// function map() {
-
-	// 	//Set default locaton to SATX
-	// 	var defLocation = new google.maps.LatLng(29.426791, -98.489602);
-
-	// 	//map options
-	// 	var mapOptions = {
-	// 		zoom: 4,
-	// 		center: defLocation
-	// 	};
-
-	// 	//Creates new map with specified map options in 'map' div
-	// 	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-	// 	//creates draggable marker at default location 
-	// 	var marker = new google.maps.Marker({
-	// 		position: defLocation,
-	// 		map: map,
-	// 		draggable: true,
-	// 		title: "Drag me!"
-	// 	});
-
-	// 	google.maps.event.addListener(marker, 'dragend', function(){
-
-	// 		//clears the weather display
-	// 		$("#insertWeather").html(" ");
-
-	// 		//Gets lat & long position assigns to variable
-	// 		var latMapMarker = this.getPosition().lat();
-	// 		var longMapMarker = this.getPosition().lng();
-
-	// 		//Makes request to openweather using lat and long from dragged marker
-	// 		var requestWeather = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-	// 			APPID: "6ac2f305a43f171cb8e8ad2076b9a183" , 
-	// 			lat: latMapMarker ,
-	// 			lon: longMapMarker,
-	// 			units: "imperial",
-	// 			cnt: 3
-	// 		})
-
-	// 		//If the request is succusessful, it will call the addWeatherToPage function
-	// 		requestWeather.done(function(data){
-	// 			console.log(data);
-	// 			// $("#cityName").html(data.city.name);
-	// 			addCityName(data.city.name);
-	// 			addWeatherToPage(data);
-	// 		})
-
-	// 		//If the request fails, error and status will be console logged
-	// 		requestWeather.fail(function(jqXHR, status, error){
-	// 			console.log(status);
-	// 			console.log(error);
-	// 		});
-	// 	});
-		
-	// };
-	// 	map();
-
 
 /*------------------------------------------------------------------------------------------------
-Search Bar Autocomplete
+Search Bar STUFF
 ------------------------------------------------------------------------------------------------*/
+		
 		//Set default locaton to SATX
 		var defLocation = new google.maps.LatLng(29.426791, -98.489602);
 
@@ -121,6 +63,7 @@ Search Bar Autocomplete
 		var input = document.getElementById('searchBox');
 		var searchBoxInput = new google.maps.places.SearchBox(input);
 
+		var marker;
 
 		$(input).keypress(function(e) {
 			if(e.which == 13) {
@@ -137,36 +80,114 @@ Search Bar Autocomplete
 
 						$("#insertWeather").html(" ");
 
-						var requestWeather = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-							APPID: "6ac2f305a43f171cb8e8ad2076b9a183" , 
-							lat: latitude ,
-							lon: longitude,
-							units: "imperial",
-							cnt: 3
-						})
+						getWeather(latitude, longitude, results[0].formatted_address);
 
-						//If the request is succusessful, it will call the addWeatherToPage function
-						requestWeather.done(function(data){
-							console.log(data);
-							addCityName(results[0].formatted_address);
-							addWeatherToPage(data);
-						})
 
-						//If the request fails, error and status will be console logged
-						requestWeather.fail(function(jqXHR, status, error){
-							console.log(status);
-							console.log(error);
-						});	
 
 						map.setCenter(results[0].geometry.location);
-							var marker = new google.maps.Marker({
-							map: map,
-							position: results[0].geometry.location	
-						});
+
+						if(!marker) {
+							marker = new google.maps.Marker({
+								map: map,
+								position: results[0].geometry.location	
+							});
+						}else {
+							marker.setPosition(results[0].geometry.location);
+						}
+
 					} else { 
 						console.log("Geocode Error" + status);
 					}
 				});
 			}
 		});	
+
+
+		function getWeather(latitude, longitude, formatAddress){
+			var requestWeather = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+				APPID: "6ac2f305a43f171cb8e8ad2076b9a183" , 
+				lat: latitude ,
+				lon: longitude,
+				units: "imperial",
+				cnt: 3
+			})
+
+			//If the request is succusessful, it will call the addWeatherToPage function
+			requestWeather.done(function(data){
+				console.log(data);
+				addCityName(formatAddress);
+				addWeatherToPage(data);
+			})
+
+			//If the request fails, error and status will be console logged
+			requestWeather.fail(function(jqXHR, status, error){
+				console.log(status);
+				console.log(error);
+			});	
+			
+		}
+
+// //draggable weather map
+
+// Sets up orginal map
+// 	function map() {
+
+// 		//Set default locaton to SATX
+// 		var defLocation = new google.maps.LatLng(29.426791, -98.489602);
+
+// 		//map options
+// 		var mapOptions = {
+// 			zoom: 4,
+// 			center: defLocation
+// 		};
+
+// 		//Creates new map with specified map options in 'map' div
+// 		var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+// 		//creates draggable marker at default location 
+// 		var marker = new google.maps.Marker({
+// 			position: defLocation,
+// 			map: map,
+// 			draggable: true,
+// 			title: "Drag me!"
+// 		});
+
+// 		google.maps.event.addListener(marker, 'dragend', function(){
+
+// 			//clears the weather display
+// 			$("#insertWeather").html(" ");
+
+// 			//Gets lat & long position assigns to variable
+// 			var latMapMarker = this.getPosition().lat();
+// 			var longMapMarker = this.getPosition().lng();
+
+// 			//Makes request to openweather using lat and long from dragged marker
+// 			var requestWeather = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+// 				APPID: "6ac2f305a43f171cb8e8ad2076b9a183" , 
+// 				lat: latMapMarker ,
+// 				lon: longMapMarker,
+// 				units: "imperial",
+// 				cnt: 3
+// 			})
+
+// 			//If the request is succusessful, it will call the addWeatherToPage function
+// 			requestWeather.done(function(data){
+// 				console.log(data);
+// 				// $("#cityName").html(data.city.name);
+// 				addCityName(data.city.name);
+// 				addWeatherToPage(data);
+// 			})
+
+// 			//If the request fails, error and status will be console logged
+// 			requestWeather.fail(function(jqXHR, status, error){
+// 				console.log(status);
+// 				console.log(error);
+// 			});
+// 		});
+		
+// 	};
+// 		map();
+
+
+
 })();
