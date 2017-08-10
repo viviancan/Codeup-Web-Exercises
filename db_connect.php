98a8532e4461176e7791231a0eeb560c2d1c4707
Vivian <vcnales@gmail.com>
2017-08-10 14:12:52 -0500

    db_connect: added constants vs. variables + sample test file

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,8 @@
 
 require_once "env.php";
 
-$dbc = new PDO("mysql:host=127.0.0.1;dbname=$dbName", $username, $password);
+
+$dbc = new PDO("mysql:host=127.0.0.1;dbname=" . DBNAME, USERNAME, PASSWORD);
 
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
---
+++ pdo_test.php
@@ -0,0 +1,14 @@
+<?php 
+require_once 'db_connect.php';
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+ 
+
+
+
+
+
+
+
+
+ ?>
