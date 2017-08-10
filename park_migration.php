7ab87baf3dd0db00451e9161d391d3b44e692896
Vivian <vcnales@gmail.com>
2017-08-10 17:07:36 -0500

    commit after walkthrough

--- park_migration.php
+++ park_migration.php
@@ -7,12 +7,13 @@ $dbc->exec("DROP TABLE IF EXISTS national_parks");
 
 $query = "CREATE TABLE IF NOT EXISTS national_parks (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT, 
-	name VARCHAR(250),
-	location VARCHAR(250),
-	date_established DATE,
-	area FLOAT(14, 2),
+	name VARCHAR(250) NOT NULL,
+	location VARCHAR(250) NOT NULL,
+	date_established DATE NOT NULL,
+	area FLOAT(14, 2) NOT NULL,
 	PRIMARY KEY (id)
 );";
 
 
 $dbc->exec($query);
+echo "parks table created" . PHP_EOL; 
