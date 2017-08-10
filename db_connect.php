d55c37822680684c65661f80286f53c946931cff
Vivian <vcnales@gmail.com>
2017-08-10 14:22:33 -0500

    removed echo

--- db_connect.php
+++ db_connect.php
@@ -3,10 +3,10 @@
 require_once "env.php";
 
 
-$dbc = new PDO("mysql:host=127.0.0.1;dbname=" . DBNAME, USERNAME, PASSWORD);
+$dbc = new PDO("mysql:host=". HOST . ";dbname=" . DBNAME, USERNAME, PASSWORD);
 
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+// echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
 
  ?>
