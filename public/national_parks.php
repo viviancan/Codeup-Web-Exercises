0c7ab1f29d1aa92b1d0a096a24c0d7af47b37a9c
Vivian <vcnales@gmail.com>
2017-08-14 11:55:53 -0500

    added BS modal for national park input

--- public/national_parks.php
+++ public/national_parks.php
@@ -6,14 +6,24 @@
 
 	print_r($_POST);
 
+
+	function getTotalCount($dbc){
+
+	}
+
+
+
+
 	function addParkToDatabase($dbc){
-		$insert = "INSERT INTO np_details (name, location, date_established, area)
-			VALUES(:park_name, :park_location, :date_established, :area)";
+		$insert = "INSERT INTO np_details (name, location, date_established, area, type)
+			VALUES(:park_name, :park_location, :date_established, :area, :type)";
 		$stmt = $dbc->prepare($insert);
 		$stmt->bindValue(':park_name', $_POST['park_name'], PDO::PARAM_STR);
 		$stmt->bindValue(':park_location', $_POST['park_location'], PDO::PARAM_STR);
 		$stmt->bindValue(':date_established', $_POST['date_established'], PDO::PARAM_STR);
 		$stmt->bindValue(':area', $_POST['area'], PDO::PARAM_STR);	
+		$stmt->bindValue(':type', $_POST['type'], PDO::PARAM_STR);	
+
 
 		$stmt->execute();	
 	}
@@ -28,17 +38,13 @@
 		$page = Input::get('page',1);
 		$offset = ($page - 1) * $limit; 
 
-		// $query = "SELECT * FROM np_details LIMIT $limit OFFSET $offset ;";			
-
 		$query = "SELECT * FROM np_details LIMIT :limit OFFSET :offset";
 
 		$stmt = $dbc->prepare($query);
-		$stmt->bindValue(':limit', $limit, PDO::PARAM_INT);
-		$stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+		$stmt->bindValue(':limit', (int) $limit, PDO::PARAM_INT);
+		$stmt->bindValue(':offset', (int) $offset, PDO::PARAM_INT);
 
 		$queryTotal = "SELECT * FROM np_details";
-
-		// $stmt = $dbc->query($query);
 
 		$stmt->execute();
 
@@ -103,31 +109,48 @@
 				</div>
 			</div>
 		<?php endif ?>
+
 		<header>
 			<div id='heading'>
-				<a href="http://codeup.dev/national_parks.php"><h1 id='heading'>NATIONAL PARKS</h1></a>
-
-				<form method="POST" action="#">
-					<label for="park_name">Park Name: </label>
-					<input type="text" name="park_name" input="park_name" placeholder="Park Name" autofocus>
-
-					<label for="park_location">Park Location: </label>
-					<input type="text" name="park_location" input="park_location" placeholder="Park State">
-
-					<label for="date_established">Date Established: </label>
-					<input type="text" name="date_established" input="date_established" placeholder="YYYY-MM-DD">
-
-					<label for="area">Park Area: </label>
-					<input type="text" name="area" input="area" placeholder="Park Area">
-
-					<button type="submit">Submit</button>
-					
-				</form>
-
-
-
+				<a href="http://codeup.dev/national_parks.php"><h1 id='heading'>NATIONAL PARKS DATABASE</h1></a>
+				<a href="#" data-toggle="modal" data-target="#parkModal"><button type="button" class="btn btn-primary btn-lg">Add a National or State Park</button></a>
 			</div>	
 		</header>
+
+
+		<div class="modal" id="parkModal">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button class="close" type="button" data-dismiss="modal">x</button>
+						<h2>Add a National or State Park</h2>
+					</div>
+					<div class="modal-body">
+						<form method="POST" action="#">
+							<label for="park_name">Park Name: </label>
+							<input type="text" name="park_name" input="park_name" placeholder="Park Name" autofocus>
+
+							<label for="park_location">Park Location: </label>
+							<input type="text" name="park_location" input="park_location" placeholder="Park State">
+
+							<label for="date_established">Date Established: </label>
+							<input type="text" name="date_established" input="date_established" placeholder="YYYY-MM-DD">
+
+							<label for="area">Park Area: </label>
+							<input type="text" name="area" input="area" placeholder="Park Area">
+
+
+							<label for="area">Park Type: </label>
+							<input type="radio" name="type" value="National">National
+							<input type="radio" name="type" value="State">State
+
+							<button type="submit">Submit</button>
+							
+						</form>	
+					</div>
+				</div>	
+			</div>	
+		</div>		
 
 		<hr>
 
