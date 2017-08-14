0bd52d1d191a7ccc596272af89dadedce1cc5cbc
Vivian <vcnales@gmail.com>
2017-08-14 14:52:55 -0500

    first refactor

--- public/national_parks.php
+++ public/national_parks.php
@@ -7,24 +7,50 @@
 	print_r($_POST);
 
 
-	function getTotalCount($dbc){
+	function addParkToDatabase($dbc){
+
+		$name = Input::get('name');
+		$location = Input::get('location');
+		$date_established = Input::get('date_established');
+		$area = Input::get('area');
+		$description = Input::get('description');
+		$type = Input::get('type');
+
+		if(!is_numeric($area)) {
+			echo "Area must be numeric";
+			return;
+		}
+
+		$insert = "
+			INSERT INTO np_details (name, location, date_established, area, description, type)
+			VALUES(:name, :location, :date_established, :area, :description, :type);";
+
+		$stmt = $dbc->prepare($insert);
+
+		$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+		$stmt->bindValue(':location', $location, PDO::PARAM_STR);
+		$stmt->bindValue(':date_established', $date_established, PDO::PARAM_STR);
+		$stmt->bindValue(':area', $area, PDO::PARAM_STR);	
+		$stmt->bindValue(':description', $description, PDO::PARAM_STR);	
+		$stmt->bindValue(':type', $type, PDO::PARAM_STR);	
+
+		$stmt->execute();	
 
 	}
 
+	function getTotalCount($dbc){
+		$countQuery = "SELECT COUNT(*) FROM np_details";
+
+		$stmt = $dbc->query($countQuery);
+
+		$count = (int) $stmt->fetchColumn();
+
+		return $count;
+	}
 
 
-
-	function addParkToDatabase($dbc){
-		$insert = "INSERT INTO np_details (name, location, date_established, area, type)
-			VALUES(:park_name, :park_location, :date_established, :area, :type)";
-		$stmt = $dbc->prepare($insert);
-		$stmt->bindValue(':park_name', Input::get('park_name'), PDO::PARAM_STR);
-		$stmt->bindValue(':park_location', Input::get('park_location'), PDO::PARAM_STR);
-		$stmt->bindValue(':date_established', Input::get('date_established'), PDO::PARAM_STR);
-		$stmt->bindValue(':area', Input::get('area'), PDO::PARAM_STR);	
-		$stmt->bindValue(':type', Input::get('type'), PDO::PARAM_STR);	
-
-		$stmt->execute();	
+	function showAllParks($dbc){
+		
 
 	}
 
@@ -38,27 +64,24 @@
 		$page = Input::get('page',1);
 		$offset = ($page - 1) * $limit; 
 
-		$query = "SELECT * FROM np_details LIMIT :limit OFFSET :offset";
-
+		$query = "SELECT * FROM np_details ORDER BY name LIMIT :limit OFFSET :offset";
 		$stmt = $dbc->prepare($query);
 		$stmt->bindValue(':limit', (int) $limit, PDO::PARAM_INT);
 		$stmt->bindValue(':offset', (int) $offset, PDO::PARAM_INT);
-
-		$queryTotal = "SELECT * FROM np_details";
-
 		$stmt->execute();
 
 		$data = [
 
 			'results' => $stmt->fetchALL(PDO::FETCH_NUM),
-			'page' => $page
+			'page' => $page,
+			'parksCount' => getTotalCount($dbc)
 
 		];
 
 		if(!empty($_POST)){
 			addParkToDatabase($dbc);
 			header("Location: http://codeup.dev/national_parks.php");
-		} 
+		}  
 
 		return $data;
 	}
@@ -125,7 +148,15 @@
 
 		<header>
 			<div id='heading'>
+
+				<button type="button" class="btn btn-primary btn-lg">View All Results</button>
+
+				<a href=""><button type="button" class="btn btn-primary btn-lg">View 4 per page</button></a>
+
+				<button type="button" class="btn btn-primary btn-lg">View 6 per page</button>
+
 				<a href="#" data-toggle="modal" data-target="#parkModal"><button type="button" class="btn btn-primary btn-lg">Add a National or State Park</button></a>
+
 			</div>	
 
 			<div class="modal" id="parkModal">
@@ -136,24 +167,28 @@
 							<h2>Add a National or State Park</h2>
 						</div>
 						<div class="modal-body">
-							<form method="POST">
-								<label for="park_name">Park Name: </label>
-								<input type="text" name="park_name" input="park_name" placeholder="Park Name" autofocus>
+							<form method="POST" action="national_parks.php">
+								<label for="name">Park Name</label>
+								<input class="form-control" type="text" name="name" input="name" placeholder="Park Name" autofocus required>
+								
+								<label for="location">Park Location</label>
+								<input class="form-control" type="text" name="location" input="location" placeholder="Park State" required>
+								
+								<label for="date_established">Date Established</label>
+								<input class="form-control" type="date" name="date_established" input="date_established" required>
+								
+								<label for="area">Park Area</label>
+								<input class="form-control" type="text" name="area" input="area" placeholder="Park Area" required>
+								
+								<label for="description">Description</label>
+								<textarea class="form-control" type="textarea" rows="3" name="description" placeholder="Please enter description here...." required></textarea>
+
+								<label for="type">Park Type</label>
 								<br>
-								<label for="park_location">Park Location: </label>
-								<input type="text" name="park_location" input="park_location" placeholder="Park State">
-								<br>
-								<label for="date_established">Date Established: </label>
-								<input type="text" name="date_established" input="date_established" placeholder="YYYY-MM-DD">
-								<br>
-								<label for="area">Park Area: </label>
-								<input type="text" name="area" input="area" placeholder="Park Area">
-								<br>
-								<label for="type">Park Type: </label>
 								<input type="radio" name="type" value="National">National
 								<input type="radio" name="type" value="State">State
-
-								<button type="submit">Submit</button>
+								<br>
+								<button class="pull-right" type="submit">Submit</button>
 								
 							</form>	
 						</div>
